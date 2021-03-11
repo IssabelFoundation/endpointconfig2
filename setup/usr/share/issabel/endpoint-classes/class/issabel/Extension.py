501958ce4a689df0cbc6e5d35257f66afc502870
Nicolas Gudino <asternic@gmail.com>
2021-03-11 16:43:06 -0500

    Read server port from config tables instead of hardcoding 5060. Needed for provisioning both SIP and PJSIP accounts

--- setup/usr/share/issabel/endpoint-classes/class/issabel/Extension.py
+++ setup/usr/share/issabel/endpoint-classes/class/issabel/Extension.py
@@ -62,7 +62,31 @@ class Extension:
                         (exten,))
             for row in sth.fetchall():
                 setattr(self, row[0], row[1])
-        
+
+        # Set port from {tech}settings table
+        if tech == 'sip':
+            sth.execute('SELECT data FROM asterisk.sipsettings WHERE keyword =' +
+                        " 'bindport' limit 1")
+            row = sth.fetchone()
+            if row is None:
+                setattr(self, 'port', '5060')
+            else:
+                setattr(self, 'port', row[0])
+        elif tech == 'pjsip':
+            sth.execute('SELECT data from asterisk.pjsipsettings where keyword =' +
+                        " 'bindport' limit 1")
+            row = sth.fetchone()
+            if row is None:
+                setattr(self, 'port', '5066')
+            else:
+                setattr(self, 'port', row[0])
+        elif tech == 'iax2':
+            sth.execute('SELECT data from asterisk.iaxsettings where keyword =' +
+                        " 'bindport' limit 1")
+            if row is None:
+                setattr(self, 'port', '4569')
+            else:
+                setattr(self, 'port', row[0])
         sth.execute(
             'SELECT property_key, property_value ' +
             'FROM endpoint_account_properties, endpoint_account ' +
